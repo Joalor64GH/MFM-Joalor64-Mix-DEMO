4ef245754c5374aa923730e58e0f1e6b9b3d5a24
Joalor64 <rhgarcon@gmail.com>
2022-05-30 13:47:37 -0400

    all of the assets

---
+++ example_mods/mfm-j64-mix-demo/stages/church_dark.lua
@@ -0,0 +1,19 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('church_dark', 'stages/church_dark', -600, -600);
+	setLuaSpriteScrollFactor('church_dark', 1, 1);
+	
+	addLuaSprite('church_dark', false);
+	scaleLuaSprite('church_dark',1.5,1.5);
+
+end
+
+function onMoveCamera(focus)
+    if focus == 'dad' then
+        setProperty('camFollow.y', getProperty('camFollow.y') -50);
+        setProperty('camFollow.x', getProperty('camFollow.x') +200);
+    elseif focus == 'boyfriend' then
+        setProperty('camFollow.y', getProperty('camFollow.y') -200);
+        setProperty('camFollow.x', getProperty('camFollow.x') -300);
+    end
+end
---
+++ example_mods/mfm-j64-mix-demo/stages/church_grey.lua
@@ -0,0 +1,19 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('church_grey', 'stages/church_grey', -600, -600);
+	setLuaSpriteScrollFactor('church_grey', 1, 1);
+	
+	addLuaSprite('church_grey', false);
+	scaleLuaSprite('church_grey',1.5,1.5);
+
+end
+
+function onMoveCamera(focus)
+    if focus == 'dad' then
+        setProperty('camFollow.y', getProperty('camFollow.y') -50);
+        setProperty('camFollow.x', getProperty('camFollow.x') +200);
+    elseif focus == 'boyfriend' then
+        setProperty('camFollow.y', getProperty('camFollow.y') -200);
+        setProperty('camFollow.x', getProperty('camFollow.x') -300);
+    end
+end
